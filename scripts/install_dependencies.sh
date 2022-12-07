07fbfb313abb1579be02cf4897d25a69e26ad67b
wszy5 <53046230+wszy5@users.noreply.github.com>
2022-12-07 13:31:55 -0800

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 yum update
-yum install mariadb-server
-mariadb
-CREATE DATABASE boards;
-CREATE USER 'wiktor'@'localhost' identified by 'ubuntu';
-GRANT ALL on boards.* to 'wiktor'@'localhost' identified by 'ubuntu';
-EXIT;
-git clone https://github.com/wszy5/focalboard-app.git
+
